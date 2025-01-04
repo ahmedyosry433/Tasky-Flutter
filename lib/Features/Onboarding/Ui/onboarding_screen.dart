6e4b8d1e7ab3e20b49a3982d38cb86bb80f1414c
Ahmed Elesawy <56431746+ahmedyosry433@users.noreply.github.com>
2025-01-04 09:25:34 +0200

    refactor onboarding

--- lib/Features/Onboarding/Ui/onboarding_screen.dart
+++ lib/Features/Onboarding/Ui/onboarding_screen.dart
@@ -12,34 +12,45 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
           children: [
-            Image.asset('assets/image/onboarging_img.png'),
-            verticalSpace(10),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: Column(
-                children: [
-                  Text(
-                    textAlign: TextAlign.center,
-                    "Task Management &\nTo-Do List",
-                    style: TextStyles.font24BlackBold,
-                  ),
-                  verticalSpace(10),
-                  Text(
-                    textAlign: TextAlign.center,
-                    "This productive tool is designed to helpyou better manage your task project-wise conveniently!",
-                    style: TextStyles.font14GrayRegular,
-                  ),
-                  AppTextButton(
+            Image.asset(
+              'assets/image/onboarging_img.png',
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 50.h,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: Column(
+                  children: [
+                    Text(
+                      "Task Management &\nTo-Do List",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.font24BlackBold,
+                    ),
+                    verticalSpace(10),
+                    Text(
+                      "This productive tool is designed to help you better manage your task project-wise conveniently!",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.font14GrayRegular,
+                    ),
+                    verticalSpace(20),
+                    AppTextButton(
                       imagePath: "assets/image/arrow_left.png",
-                      buttonText: "Let’s Start",
+                      buttonText: "Let's Start",
                       textStyle: TextStyles.font14WhiteSemiBold,
                       onPressed: () {
                         context.pushNamed(Routes.registerScreen);
-                      })
-                ],
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
           ],
