a299ce2c25a410a81c5a09a7bce9530165166f5e
Ahmed Elesawy <56431746+ahmedyosry433@users.noreply.github.com>
2025-01-09 11:45:32 +0200

    login clean✅

--- lib/Features/Login/Ui/login_screen.dart
+++ lib/Features/Login/Ui/login_screen.dart
@@ -28,102 +28,123 @@ class _LoginScreenState extends State<LoginScreen> {
           height: MediaQuery.of(context).size.height,
           child: Stack(
             children: [
-              Image.asset(
-                'assets/image/onboarging_img.png',
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
+              _buildBackgroundImage(),
               verticalSpace(14),
               Positioned(
                 bottom: 40.h,
                 left: 0,
                 right: 0,
-                child: Form(
-                  key: BlocProvider.of<LoginCubit>(context).loginFormKey,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20.w),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Login',
-                              textAlign: TextAlign.right,
-                              style: TextStyles.font24BlackBold,
-                            ),
-                            verticalSpace(15),
-                            IntlPhoneField(
-                              decoration:
-                                  DecorationStyle.inputDecoration.copyWith(
-                                hintText: '123 456-7890',
-                              ),
-                              initialCountryCode: 'EG',
-                              onChanged: (phone) {
-                                BlocProvider.of<LoginCubit>(context)
-                                    .phoneController
-                                    .text = phone.completeNumber;
-                              },
-                            ),
-                            verticalSpace(10),
-                            AppTextFormField(
-                              maxLines: 1,
-                              controller: BlocProvider.of<LoginCubit>(context)
-                                  .passwordController,
-                              hintText: "Password",
-                              validator: (value) {},
-                              keyboardType: TextInputType.visiblePassword,
-                              isObscureText: _isPasswordVisible,
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _isPasswordVisible
-                                      ? Icons.visibility_off
-                                      : Icons.visibility,
-                                  color: Colors.grey,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _isPasswordVisible = !_isPasswordVisible;
-                                  });
-                                },
-                              ),
-                            ),
-                            verticalSpace(15),
-                            AppTextButton(
-                                buttonText: "Sign in",
-                                textStyle: TextStyles.font16WhiteBold,
-                                onPressed: () {
-                                  login();
-                                }),
-                            verticalSpace(14),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Didn’t have any account? ',
-                                    style: TextStyles.font14GrayRegular),
-                                GestureDetector(
-                                  onTap: () {
-                                    context.pushNamed(Routes.registerScreen);
-                                  },
-                                  child: Text(
-                                    'Sign Up here',
-                                    style: TextStyles.font12PrimaryBold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            _buildBlocLisenner(context),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                child: _buildLoginForm(),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildBackgroundImage() {
+    return Image.asset(
+      'assets/image/onboarging_img.png',
+      width: MediaQuery.of(context).size.width,
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Form(
+      key: BlocProvider.of<LoginCubit>(context).loginFormKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildLoginHeader(),
+                verticalSpace(15),
+                _buildPhoneField(),
+                verticalSpace(10),
+                _buildPasswordField(),
+                verticalSpace(15),
+                _buildSignInButton(),
+                verticalSpace(14),
+                _buildSignUpLink(),
+                _buildBlocLisenner(context),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLoginHeader() {
+    return Text(
+      'Login',
+      style: TextStyles.font24BlackBold,
+    );
+  }
+
+  Widget _buildPhoneField() {
+    return IntlPhoneField(
+      decoration: DecorationStyle.inputDecoration.copyWith(
+        hintText: '123 456-7890',
+      ),
+      initialCountryCode: 'EG',
+      onChanged: (phone) {
+        BlocProvider.of<LoginCubit>(context).phoneController.text =
+            phone.completeNumber;
+      },
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return AppTextFormField(
+      maxLines: 1,
+      controller: BlocProvider.of<LoginCubit>(context).passwordController,
+      hintText: "Password",
+      validator: (value) {},
+      keyboardType: TextInputType.visiblePassword,
+      isObscureText: _isPasswordVisible,
+      suffixIcon: IconButton(
+        icon: Icon(
+          _isPasswordVisible ? Icons.visibility_off : Icons.visibility,
+          color: Colors.grey,
+        ),
+        onPressed: () {
+          setState(() {
+            _isPasswordVisible = !_isPasswordVisible;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildSignInButton() {
+    return AppTextButton(
+        buttonText: "Sign in",
+        textStyle: TextStyles.font16WhiteBold,
+        onPressed: () {
+          login();
+        });
+  }
+
+  Widget _buildSignUpLink() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Didn’t have any account? ', style: TextStyles.font14GrayRegular),
+        GestureDetector(
+          onTap: () {
+            context.pushNamed(Routes.registerScreen);
+          },
+          child: Text(
+            'Sign Up here',
+            style: TextStyles.font12PrimaryBold,
+          ),
+        ),
+      ],
     );
   }
 
